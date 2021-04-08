3508344797227c531db1db3d88de08536ce4d5b5
hemomaster <67165275+hemomaster@users.noreply.github.com>
2021-04-08 17:55:51 +0300

    День 3. Настроил слайдер 1, 2 каналов.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const swiper = new Swiper(".subscript__slider", {
+  const swiperChannel = new Swiper(".subscript__slider", {
     slidesPerView: 4,
     spaceBetween: 40,
     loop: true,
     navigation: {
       nextEl: ".slider-button-next",
       prevEl: ".slider-button-prev",
+    },
+  });
+  const swiperRecommended = new Swiper(".recommended__slider", {
+    slidesPerView: 2,
+    spaceBetween: 40,
+    loop: true,
+    navigation: {
+      nextEl: ".recommended__slider-button-next",
+      prevEl: ".recommended__slider-button-prev",
     },
   });
 });
